88782a4986c217c1fcd19ef489046cfb67ca3435
Юрий Федин <fedin.iua@phystech.edu>
2024-03-24 00:32:53 +0300

    start of the new day

--- Assets/Scripts/MovementControlls.cs
+++ Assets/Scripts/MovementControlls.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class MovementControlls : MonoBehaviour
 {
@@ -11,12 +8,10 @@ public class MovementControlls : MonoBehaviour
     [SerializeField] private float speed = 5;
     //private Animator animator;
 
-    private Entity entity;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         //animator = GetComponent<Animator>();
-        entity = GetComponent<Entity>();
     }
 
     // Update is called once per frame
@@ -26,16 +21,9 @@ public class MovementControlls : MonoBehaviour
     
     private void Run()
     {
-        if (entity) 
-        {
-            HorizontalSpeed = Input.GetAxisRaw("Horizontal") * speed;
-            VerticalSpeed = Input.GetAxisRaw("Vertical") * speed;
-            Vector2 TargetVelocity = new Vector2(HorizontalSpeed, VerticalSpeed);
-            rb.velocity = TargetVelocity.normalized * speed;
-            
-            entity.TrueFlip(rb);
-        }
-
-        //animator.SetFloat("IsMoving", Math.Abs(HorizontalSpeed) + Math.Abs(VerticalSpeed));
+        HorizontalSpeed = Input.GetAxisRaw("Horizontal") * speed;
+        VerticalSpeed = Input.GetAxisRaw("Vertical") * speed;
+        Vector2 TargetVelocity = new Vector2(HorizontalSpeed, VerticalSpeed);
+        rb.velocity = TargetVelocity.normalized * speed;
     }
 }
