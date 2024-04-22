c3a68640ea53a7340f3b0be8600495ad4efa556c
Юрий Федин <fedin.iua@phystech.edu>
2024-04-22 22:32:43 +0300

    Изменил движение, убрал дёргание при коллизиях

--- Assets/Scripts/Controllers/PlayerMovementController.cs
+++ Assets/Scripts/Controllers/PlayerMovementController.cs
@@ -7,10 +7,11 @@ namespace Runtime
         private Animator m_Animator;
         private Vector2 Direction;
         private Vector3 MotionDirection;
-
+        private Rigidbody2D rigidbody2D;
         public void OnStart() 
         {
             m_Animator = Game.Runtime.PlayerView.GetComponent<Animator>();
+            rigidbody2D = Game.Runtime.PlayerView.GetComponent<Rigidbody2D>();
         }
 
         public void OnStop() 
@@ -40,7 +41,11 @@ namespace Runtime
             m_Animator.SetFloat("Vertical", moveY);
             m_Animator.SetFloat("Speed", speed);
 
-            Game.Runtime.PlayerView.transform.Translate(MotionDirection * Game.Runtime.PlayerData.speed * Time.deltaTime);
+            Vector3 Pos = Game.Runtime.PlayerView.transform.position;
+            Vector3 NewPos = Pos + Game.Runtime.PlayerData.speed * Time.fixedDeltaTime * MotionDirection;
+
+            rigidbody2D.MovePosition(rigidbody2D.position + Direction.normalized*Game.Runtime.PlayerData.speed*Time.fixedDeltaTime);
+            // Game.Runtime.PlayerView.transform.position = Vector3.MoveTowards(Pos, NewPos, Time.deltaTime * Game.Runtime.PlayerData.speed);
         }
     }
     
